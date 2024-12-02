364892c6a8365c049a889fdcc0e0bf1d09e87413
Vernong132 <vgage@hawkmail.hccfl.edu>
2024-12-01 23:22:35 -0500

    Add files via upload

---
+++ Java5.java
@@ -0,0 +1,18 @@
+public class Java5 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
+
+// Pseudocode
+// BEGIN
+    // PRINT "Welcome to Java"
+    // PRINT "Welcome to Java"
+    // PRINT "Welcome to Java"
+    // PRINT "Welcome to Java"
+    // PRINT "Welcome to Java"
+// END
